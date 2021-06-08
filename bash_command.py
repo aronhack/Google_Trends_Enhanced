4d6d75f8f3df9fabc6ed7a25338d735534870c65
aronhack <48109999+aronhack@users.noreply.github.com>
2021-06-09 01:56:32 +0800

    Update

---
+++ bash_command.py
@@ -0,0 +1,14 @@
+
+
+
+source /home/aronhack/google_trends_enhanced/virtualenv/bin/activate
+
+
+
+
+
+
+pip install -r /home/aronhack/google_trends_enhanced/virtualenv/requirements3.txt
+
+cd /home/aronhack/google_trends_enhanced/virtualenv
+pip install -r requirements.txt
